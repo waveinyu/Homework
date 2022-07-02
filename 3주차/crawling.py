ca628f833192933b7ac547687a8e5f788b037ff3
Taesik yoon <taesikyoon97@naver.com>
2022-07-02 20:43:08 +0900

    3주차

--- 3주차/crawling.py
+++ 3주차/crawling.py
@@ -1,15 +1,33 @@
 # https://www.genie.co.kr/chart/top200?ditc=M&rtm=N&ymd=20210701 
 
+from tempfile import SpooledTemporaryFile
 import requests
 from bs4 import BeautifulSoup
 
 # 타겟 URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('크롤링할 URL',headers=headers)
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=M&rtm=N&ymd=20210701',headers=headers)
 
 # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
 # soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
 # 이제 코딩을 통해 필요한 부분을 추출하면 된다.
 soup = BeautifulSoup(data.text, 'html.parser')
 
-#check
+
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number 순위
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis 타이틀 제목
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis 가수이름
+# .strip() 공백을 지워주는 함수
+
+# rank =soup.select('td.number')
+# print(rank.text) 실패 기억안남
+
+
+all_list = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for song in all_list:
+    rank = song.select_one('td.number').text[0:2].strip()
+    title = song.select_one('td.info > a.title.ellipsis').text.strip()
+    artist = song.select_one('td.info > a.artist.ellipsis').text.strip()
+    print(rank,title,artist)
+
