da23024e6199ef0942f607e8de4b8e32cefc4b36
Taesik yoon <taesikyoon97@naver.com>
2022-07-02 21:52:32 +0900

    4주차

--- 4주차/app.py
+++ 4주차/app.py
@@ -1,22 +1,38 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://taesikyoon97:louis17467@cluster0.ncjxm.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
-sample_receive = request.form['sample_give']
-print(sample_receive)
-return jsonify({'msg':'POST 연결 완료!'})
+    user_name = request.form['name_give']
+    user_comment = request.form['commnet_give']
+    doc = { 
+        "name": user_name,
+        "comment": user_comment
+    }
+    db.users.insert_one(doc)
+    print(user_name,user_comment)
+    return jsonify({'msg': 'DB 저장 완료'})
+
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-return jsonify({'msg':'GET 연결 완료!'})
+    comment_user = list(db.users.find({},{'_id':False}))
+    return jsonify({'comments':comment_user})
+
 
 if __name__ == '__main__':
-app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
 
 
 #check
